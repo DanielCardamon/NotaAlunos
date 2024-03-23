97814aee61c6283c3841e2b73e2b0db3fc7c0f5e
unknown <daniel.viegas.c@gmail.com>
2024-03-23 10:15:25 -0300

    ititial commit

---
+++ src/vetornota/VetorNota.java
@@ -0,0 +1,37 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package vetornota;
+
+import javax.swing.JOptionPane;
+
+/**
+ *
+ * @author FATEC ZONA LESTE
+ */
+public class VetorNota {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        int tot = Integer.parseUnsignedInt(JOptionPane.showInputDialog("Quantidade de alunos"));
+        float[ ] nota = new float[tot];
+        String []nome = new String[tot];
+        float soma = 0,media;
+        for( int i = 0; i<tot;i++){
+        nome[i] = JOptionPane.showInputDialog("Digite nome do aluno "+ (i+1));
+        nota[i]=Float.parseFloat(JOptionPane.showInputDialog("Nota do " + nome[i]));
+        soma+=nota[i];
+        }
+        media = soma/tot;
+        for( int i = 0; i<tot;i++){
+            if(nota[i]> media)
+                System.out.println("Parabems "+nome[i]);
+        } 
+        
+    }
+    
+}
